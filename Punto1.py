b6f1ed1e45881a47330ef83b2045384ee13ed574
Luisa Toro Villegas <ltorov@eafit.edu.co>
2024-02-26 17:24:09 -0500

    Update Punto1.py

---
+++ Punto1.py
@@ -0,0 +1,19 @@
+from StandardBrownianMotion import StandardBrownianMotion
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_trayectories = 1000
+num_steps = 500
+
+brownian_motions = StandardBrownianMotion(num_trayectories, num_steps, max_time = 1/4)
+brownian_motions.generate_brownian_motion()
+
+t = brownian_motions.t
+
+diffs = np.zeros(num_steps)
+
+for step in range(1,num_steps):
+    diffs [step] = (1-4*t[step])**(-1/2)-np.mean(np.exp(2*brownian_motions.brownian_motion[step,:]**2))
+
+plt.plot(t, diffs)
+plt.show()
