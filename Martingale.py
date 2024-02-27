c2a0736f6287fbf5d2095b7f7a054d8ba40e5edf
Luisa Toro Villegas <ltorov@eafit.edu.co>
2024-02-27 17:15:55 -0500

    Update Martingale.py

--- Martingale.py
+++ Martingale.py
@@ -33,6 +33,7 @@ class MartingaleSimulation:
         while self.balance > 0:
             if self.max_rounds != None and self.rounds_played >= self.max_rounds:
                 break
+            
             if self.max_bet_amount != None and self.bet_amount >= self.max_bet_amount:
                 break
 
