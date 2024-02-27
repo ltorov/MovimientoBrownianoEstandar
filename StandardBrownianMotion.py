347ec4e5d540dcd45a0cb99dba514b64127b10ba
Andres Gomez <andres.gomeza@perficient.com>
2024-02-27 14:09:12 -0500

    # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	new file:   .flake8 #	new file:   .vscode/settings.json #	modified:   StandardBrownianMotion.py #

--- StandardBrownianMotion.py
+++ StandardBrownianMotion.py
@@ -1,25 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class StandardBrownianMotion:
-    def __init__(self, num_trayectories, num_steps, max_time = 1):
+    def __init__(self, num_trayectories, num_steps, max_time=1):
         self.num_trayectories = num_trayectories
         self.num_steps = num_steps
         self.dt = max_time/num_steps
         self.t = np.linspace(0, max_time, num_steps)
+        self.brownian_motions = None
 
     def generate_brownian_motion(self):
         brownian_motions = np.zeros((self.num_trayectories, self.num_steps))
 
         for i in range(self.num_trayectories):
             for j in range(1, self.num_steps):
-                brownian_motions[i,j] = brownian_motions[i,j-1] + np.random.normal(0, np.sqrt(self.dt))
+                brownian_motions[i, j] = brownian_motions[i, j-1] + np.random.normal(0, np.sqrt(self.dt))
 
-        self.brownian_motion = brownian_motions
-    
+        self.brownian_motions = brownian_motions
+
     def plot(self):
-        for i in range(len(self.brownian_motion)):
-            plt.plot(self.t, self.brownian_motion[i, :])
+        for i in range(len(self.brownian_motions)):
+            plt.plot(self.t, self.brownian_motions[i, :])
         plt.title('Movimiento Browniano Estándar')
         plt.xlabel('Tiempo')
         plt.show()
